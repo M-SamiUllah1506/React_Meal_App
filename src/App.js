34369fd4b0d3ab1aef89f018023796e295c4d898
Muhammad Sami Ullah <sami.suu1506@gmail.com>
2025-07-29 23:41:31 +0500

    Night Time

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import './App.css';
 import React,{Fragment} from 'react';
 import Header from './Components/UI/Layout/Header';
 import Meals from './Meals/Meal';
+import Cart from './Meals/Cart/Cart';
 function App() {
   return (
     <Fragment>
+    <Cart/>
       <Header/>
       <main>
          <Meals/>
